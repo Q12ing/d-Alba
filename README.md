cf734adcd2ba969ad4ebb148716b6fa9610afc4d
Q12ing <simjang640@gmail.com>
2025-02-07 11:00:22 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -35,24 +35,26 @@ font-size:1.375rem; line-height:1.3; letter-spacing:-0.02em;
 font-size:1.375rem; font-weight:700; line-height:1.5; letter-spacing:-0.02em;
 ### 콜렉션메뉴강조 (d) 24
 font-size:1.5rem; font-weight:600; letter-spacing:-0.02em;
-### 소제목 (d) 32
-font-size:2rem; font-weight:800; letter-spacing:-0.02em;
 
 ## Hanken Grotesk
+font-family:"hanken","pretendard",sans-serif;
 ### 버튼 (d) 14
 font-size:0.875rem; letter-spacing:-0.02em;
 ### 미니배너타이틀 (d) 26
 font-size:1.625rem; font-weight:500; letter-spacing:-0.02em;
 ### 콜렉션가격강조 (d) 28
 font-size:1.75rem; font-weight:600; letter-spacing:-0.02em;
+### 소제목 (d) 28
+font-size:1.75rem; font-weight:700; letter-spacing:-0.02em;
 ### 중제목 (d) 48
 font-size:3rem; font-weight:700; line-height:1.3; letter-spacing:-0.04em;
 ### 배너타이틀 (d) 64
 font-size:4rem; font-weight:900; letter-spacing:-0.02em;
 
 ## Gloock
+font-family:"gloockR","pretendard",sans-serif;
 ### 할인 (d) 20
-font-size:0.25rem; letter-spacing:-0.02em;
+font-size:1.25rem; letter-spacing:-0.02em;
 
 
 ## Color Style
--- script/index.js
+++ script/index.js
@@ -1,1 +1,45 @@
-const bsAllSwiper = new Swiper('.bs_all_swiper')
+/* main bnr swiper */
+const mainDot = document.querySelectorAll('.main_bnr .main_dot li');
+const mainSwiper = new Swiper('.main_swiper',{
+    autoplay:{
+        delay:3000,
+    },
+    loop:true,
+    effect:'fade',
+    on: {
+        slideChange: function (e) {
+            // 모든 dot에서 dot_active 클래스 제거
+            mainDot.forEach(dot => dot.classList.remove('dot_active'));
+            // 현재 활성화된 슬라이드에 맞는 dot에 클래스 추가
+            mainDot[e.realIndex].classList.add('dot_active');
+        },
+    },
+})
+mainDot.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        mainSwiper.slideToLoop(index);
+    });
+});
+
+
+/* best, new product swiper */
+function productSettings() {
+    return {
+        slidesPerView:4,
+        spaceBetween:26
+    };
+}
+const bsAllSwiper = new Swiper('.bs_all_swiper',productSettings());
+const bsMakeUpSwiper = new Swiper('.bs_makeup_swiper',productSettings());
+const bsSkincareSwiper = new Swiper('.bs_skincare_swiper',productSettings());
+const npAllSwiper = new Swiper('.np_all_swiper',productSettings());
+const npMakeUpSwiper = new Swiper('.np_makeup_swiper',productSettings());
+const npSkincareSwiper = new Swiper('.np_skincare_swiper',productSettings());
+
+
+/* best, new product progress bar */
+const pbProgress = document.querySelectorAll ('.active_progress_bar');
+function progressBar() {
+    return {};
+}
+for(let i in pbProgress){console.log(i);}
