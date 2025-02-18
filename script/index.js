68ff7e2b9fdc3c01c78b354f316e49f9c190fdca
Q12ing <simjang640@gmail.com>
2025-02-18 18:06:48 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -22,32 +22,67 @@ mainDot.forEach((dot, index) => {
 });
 
 
+
+/* Best, New Product 탭메뉴 active 클래스 이동 */
+$('.best_menu li').click(function(){
+    $('.best_menu li').removeClass("active");
+    $(this).addClass("active");
+})
+$('.new_product .new_menu li').click(function(){
+    $('.new_product li').removeClass("active");
+    $(this).addClass("active");
+})
+/* Best, New 탭메뉴 클릭시 해당 슬라이드 보여지기 */
+const bstap=document.querySelectorAll('.best_seller .contents_title li')
+const nptap=document.querySelectorAll('.new_product .contents_title li')
+console.log(bstap, nptap)
+const bsSwiper=document.querySelectorAll('.bs_box .pb')
+const npSwiper=document.querySelectorAll('.np_box .pb')
+console.log(bsSwiper, npSwiper)
+
+function hideBsProduct() {
+    bsSwiper.forEach(view => {
+        view.style.display = 'none';
+    });
+}
+bstap.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        // 모든 Swiper 숨기기
+        hideBsProduct();
+        // 해당 index의 Swiper 보이기
+        bsSwiper[index].style.display = 'block';
+    });
+});
+
+function hideNpProduct() {
+    npSwiper.forEach(view => {
+        view.style.display = 'none';
+    });
+}
+
+nptap.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        // 모든 Swiper 숨기기
+        hideNpProduct();
+        // 해당 index의 Swiper 보이기
+        npSwiper[index].style.display = 'block';
+    });
+});
+
+
+
 /* best, new product swiper */
-/* function productSettings() {
-    return {
-        autoplay:{
-            delay:10000,
-        },
-        loop:true,
-        slidesPerView:4,
-        spaceBetween:26
-    }; 
-} */
-/* const bsAllSwiper = new Swiper('.bs_all_swiper',productSettings());
-const bsMakeUpSwiper = new Swiper('.bs_makeup_swiper',productSettings());
-const bsSkincareSwiper = new Swiper('.bs_skincare_swiper',productSettings());
-const npAllSwiper = new Swiper('.np_all_swiper',productSettings());
-const npMakeUpSwiper = new Swiper('.np_makeup_swiper',productSettings());
-const npSkincareSwiper = new Swiper('.np_skincare_swiper',productSettings());
- */
-const activeProgressBar = document.querySelectorAll('.active_progress_bar');
-const bsAllSwiper = new Swiper('.bs_all_swiper',{
-    autoplay:{
-        delay:3000,
+
+const bsAllSwiper = new Swiper('.bs_all_swiper', {
+    autoplay: {
+        delay: 3000,
     },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:26,
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 26,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 const bsMakeUpSwiper = new Swiper('.bs_makeup_swiper',{
     autoplay:{
@@ -56,6 +91,9 @@ const bsMakeUpSwiper = new Swiper('.bs_makeup_swiper',{
     loop:true,
     slidesPerView:4,
     spaceBetween:26,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 const bsSkincareSwiper = new Swiper('.bs_skincare_swiper',{
     autoplay:{
@@ -64,6 +102,9 @@ const bsSkincareSwiper = new Swiper('.bs_skincare_swiper',{
     loop:true,
     slidesPerView:4,
     spaceBetween:26,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 const npAllSwiper = new Swiper('.np_all_swiper',{
     autoplay:{
@@ -72,6 +113,9 @@ const npAllSwiper = new Swiper('.np_all_swiper',{
     loop:true,
     slidesPerView:4,
     spaceBetween:26,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 const npMakeUpSwiper = new Swiper('.np_makeup_swiper',{
     autoplay:{
@@ -80,6 +124,9 @@ const npMakeUpSwiper = new Swiper('.np_makeup_swiper',{
     loop:true,
     slidesPerView:4,
     spaceBetween:26,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 const npSkincareSwiper = new Swiper('.np_skincare_swiper',{
     autoplay:{
@@ -88,5 +135,44 @@ const npSkincareSwiper = new Swiper('.np_skincare_swiper',{
     loop:true,
     slidesPerView:4,
     spaceBetween:26,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 
+/* collection list */
+$('.collection_list .crt_content').hide();
+$('.collection_list .list_active').siblings('.crt_content').show();
+$('.crt_title').children('.crt_hover').hide();
+$('.list_active').children('.crt_hover').show();
+$('.crt_title').click(function(){
+    // 다른 항목의 active 제거 & hover 숨기기
+    $('.crt_title').removeClass("list_active");
+    $('.crt_title').children('.crt_hover').hide();
+    // 현재 클릭한 항목 활성화 & hover 표시
+    $(this).addClass("list_active");
+    $(this).children('.crt_hover').show();
+    // 모든 .crt_content 숨기기 (현재 클릭한 항목의 다음 요소는 제외)
+    $('.crt_content').not($(this).next('.crt_content')).slideUp();
+    // 현재 선택한 .crt_content가 닫혀 있으면 열기
+    if (!$(this).next('.crt_content').is(":visible")) {
+        $(this).next('.crt_content').slideDown();
+    }
+})
+const collectionView=document.querySelectorAll('.view_box')
+const collectionList=document.querySelectorAll('.collection_list li')
+function hideAllViews() {
+    collectionView.forEach(view => {
+        view.style.display = 'none';
+    });
+}
+
+// 모든 리스트 항목에 클릭 이벤트 추가
+collectionList.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        // 모든 view_box 숨기기
+        hideAllViews();
+        // 해당 index의 view_box 보이기
+        collectionView[index].style.display = 'block';
+    });
+});
--- script/product.js
+++ script/product.js
@@ -5,6 +5,34 @@ share.addEventListener('click',()=>{
     window.alert('링크가 복사되었습니다.')
 })
 
+
+/* 섬네일 반응 */
+const thumbnail = document.querySelectorAll('.thumbnail a img');
+const bigImage = document.querySelector('.photo .big img');
+console.log(thumbnail[0].src);
+function bigSrc(num){
+    return bigImage.src=`./images/b${num}.jpg`
+}
+thumbnail[0].addEventListener('mouseover',()=>{
+    bigSrc(1);
+});
+thumbnail[1].addEventListener('mouseover',()=>{
+    bigSrc(2);
+});
+thumbnail[2].addEventListener('mouseover',()=>{
+    bigSrc(3);
+});
+thumbnail[3].addEventListener('mouseover',()=>{
+    bigSrc(4);
+});
+thumbnail[4].addEventListener('mouseover',()=>{
+    bigSrc(5);
+});
+thumbnail[5].addEventListener('mouseover',()=>{
+    bigSrc(6);
+});
+
+
 /* 배송정보 */
 const schedule = document.querySelector('.schedule');
 const scheduleOpen = document.querySelector('.schedule_open');
@@ -15,21 +43,91 @@ schedule.addEventListener('click',(e)=>{
     scheduleOpen.classList.toggle('hide');
 });
 
+/* 셀렉트 옵션 */
+const selectOption = document.querySelector('#capacity');
+const op50ml=document.querySelector('.op50ml');
+const op100ml=document.querySelector('.op100ml');
+console.log (op50ml)
+console.log(selectOption.innerText)
+op50ml.style.display='none';
+op100ml.style.display='none';
+selectOption.addEventListener('change',function(){
+    let selectedValue = selectOption.value;
+    if (selectedValue == '50ml') {
+        op50ml.style.display='block'
+    }else if (selectedValue == '100ml'){
+        op100ml.style.display='block'
+    }
+})
+
 /* 총금액 */
 let price50 = 27000; //주문금액
-let number50 = 0; //주문수량
-let totalPrice50 = 0; //총 가격 저장 변수
+let number50 = 1; //주문수량
 const plusBtn50 = document.querySelector('.plus50');//수량증가DOM
 const minusBtn50 = document.querySelector('.minus50');//수량감소DOM
 const orderPrice50 = document.querySelector('.op_total50')//최종주문표시DOM
-console.log(orderPrice50)
+let op50Num=document.querySelector('#op50ml_num');
 
-orderPrice50.innerText = price50*number50;
+let price100 = 34000; //주문금액
+let number100 = 1; //주문수량
+const plusBtn100 = document.querySelector('.plus100');//수량증가DOM
+const minusBtn100 = document.querySelector('.minus100');//수량감소DOM
+const orderPrice100 = document.querySelector('.op_total100')//최종주문표시DOM
+let op100Num=document.querySelector('#op100ml_num');
+
+/* =========================================★★옵션 통합 총 금액 JS 진행 중 */
+const optionTotalPrice = document.querySelector('.order_price .total_price')
+let totalPrice = 0; //총 가격 저장 변수
+console.log(optionTotalPrice);
+optionTotalPrice.innerText=totalPrice;
+/* ==========================================★★★★★★★★★ */
 
 plusBtn50.addEventListener('click',()=>{
-    number50++;
-    orderPrice50.innerText=number50*price50;
+    number50++; // 수량 증가
+    op50Num.value = number50; // input 값 업데이트
+    let total50 = number50 * price50; // 총 금액 계산
+    orderPrice50.innerText = total50.toLocaleString('ko-kr');
+    totalPrice += price50
+    optionTotalPrice.innerText = totalPrice.toLocaleString('ko-kr');
 })
+minusBtn50.addEventListener('click',()=>{
+    if(number50==1){
+        window.alert('최소 수량은 1개 입니다.')
+    }else{
+        number50--;
+        op50Num.value = number50;
+        let total50=innerText=number50*price50;
+        orderPrice50.innerText=total50.toLocaleString('ko-kr');
+        totalPrice -= price50
+        optionTotalPrice.innerText = totalPrice.toLocaleString('ko-kr');
+    }
+})
+plusBtn100.addEventListener('click',()=>{
+    number100++;
+    op100Num.value = number100;
+    let total100=innerText=number100*price100;
+    orderPrice100.innerText=total100.toLocaleString('ko-kr');
+    totalPrice += price100
+    optionTotalPrice.innerText = totalPrice.toLocaleString('ko-kr');
+})
+minusBtn100.addEventListener('click',()=>{
+    if(number100==1){
+        window.alert('최소 수량은 1개 입니다.')
+    }else{
+        number100--;
+        op100Num.value = number100;
+        let total100=innerText=number100*price100;
+        orderPrice100.innerText=total100.toLocaleString('ko-kr');
+        totalPrice -= price100
+        optionTotalPrice.innerText = totalPrice.toLocaleString('ko-kr');
+    };
+})
+
+/* close 눌렀을 때 옵션닫기-제이쿼리 */
+$('.close').click(function(){
+    $(this).parent().parent().hide();
+})
+
 
 /* 장바구니 버튼 클릭 상호 작용 */
 const cartPopup = document.querySelector('.cart_open_bg');
@@ -40,7 +138,6 @@ cartPopup.style.display = 'none';
 //2. 장바구니 클릭시
 cartBtn.addEventListener('click',()=>{
     cartPopup.style.display = 'block'
-    document.body.style.overflow = 'hidden';
 });
 //4. 팝업 내 '쇼핑 계속하기' 클릭 시
 shoppingBtn.addEventListener('click',(e)=>{
